eb5ebaa757618e1e71372f0a9ebf073c2c5e64a0
xuanbach1992 <Xuanbach1992>
2019-11-29 16:51:38 +0700

    add

--- src/app/awesome/add/add.component.ts
+++ src/app/awesome/add/add.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AwesomeService} from '../../server/awesome.service';
+import {FormBuilder} from '@angular/forms';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-add',
@@ -6,10 +9,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add.component.css']
 })
 export class AddComponent implements OnInit {
+  addAwesomeForm = this.formBuilder.group({
+    tag: [''],
+    url: [''],
+    descriptions: [''],
+  });
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder,
+              private awesomeService: AwesomeService,
+              private router: Router) {
+  }
 
   ngOnInit() {
   }
 
+  create() {
+    const newAwesome = this.addAwesomeForm.value;
+    this.awesomeService.addSuccess(newAwesome).subscribe(res => {
+      return this.router.navigate(['/awesome']);
+    });
+  }
 }
