dc51816df8a9fe09b145b46611ec5f9b155fa141
xuanbach1992 <Xuanbach1992>
2019-11-29 16:52:17 +0700

    delete and server

--- src/app/server/awesome.service.ts
+++ src/app/server/awesome.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {IAwesome} from '../awesome/iawesome';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
@@ -16,7 +16,20 @@ export class AwesomeService {
   getAll(): Observable<IAwesome[]> {
     return this.http.get<IAwesome[]>(this.url);
   }
+
   findById(id: number): Observable<IAwesome> {
     return this.http.get<IAwesome>(this.url + '/' + id);
+  }
+
+  addSuccess(newAwesome: IAwesome): Observable<IAwesome> {
+    return this.http.post<IAwesome>(this.url , newAwesome);
+  }
+
+  deleteSuccess(id: number): Observable<IAwesome> {
+    return this.http.delete<IAwesome>(this.url + '/' + id);
+  }
+
+  updateSuccess(id: number, newAwesome: IAwesome) {
+    return this.http.put<IAwesome>(this.url + '/' + id, newAwesome);
   }
 }
